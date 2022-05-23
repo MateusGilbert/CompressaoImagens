395193c2bcbb3bb598188658525ed93325336fca
Mateus da Silva Gilbert <gilbertmateus@poli.ufrj.br>
2022-05-23 00:56:07 -0300

    Add files via upload

--- lbg_vq/header.hpp
+++ lbg_vq/header.hpp
@@ -7,6 +7,10 @@
 #include <regex>
 #include <vector>
 #include <cmath>
+#include <algorithm>
+#include <random>
+
+using namespace std;
 
 typedef vector< int* > vect_list;
 //typedef struct{
@@ -18,7 +22,6 @@ typedef vector< int* > vect_list;
 void save_csv(int*, int=0, int=0, string="out.csv");
 int* op_pgm(int&, int&, string="");
 vect_list vectorize(int*, int, int, int, int);
-
-using namespace std;
+vect_list lbg(vect_list, int, int, float=.1);
 
 #endif
--- lbg_vq/lbg.cpp
+++ lbg_vq/lbg.cpp
@@ -20,11 +20,11 @@ inline void def_qregion(int *vect, vect_list centroids, int idx, lbg_aux aux){
 	float sq_dist;
 	int cur_id=0;
 	for (int *c : centroids){
-		sq_dist = eq_dist2(vect, c, aux->size);
-		if ((sq_dist < aux->sq_dist[idx]) or (aux->cent_ind[idx] == cur_id++)){
-			aux->sq_dist[idx] = sq_dist;
-			if (aux->cent_ind[idx] != cur_id-1)
-				aux->cent_ind[idx] = cur_id - 1;
+		sq_dist = eq_dist2(vect, c, aux.size);
+		if ((sq_dist < aux.sq_dist[idx]) or (aux.cent_ind[idx] == cur_id++)){
+			aux.sq_dist[idx] = sq_dist;
+			if (aux.cent_ind[idx] != cur_id-1)
+				aux.cent_ind[idx] = cur_id - 1;
 		}
 	}
 
@@ -32,33 +32,92 @@ inline void def_qregion(int *vect, vect_list centroids, int idx, lbg_aux aux){
 }
 
 inline reg_register split_vects(lbg_aux aux){
-	reg_register reg_reg[aux->n_cent];
+	reg_register reg_reg;//[aux.n_cent];
+	for (int i=0; i<aux.n_cent; i++)
+		reg_reg.push_back(vector< int >());
 
-	for (int i=0; i<aux->n_train; i++){
-		int c_id = aux->cent_ind[i];
-		reg_reg[i].push_back(i);
+	for (int i=0; i<aux.n_train; i++){
+		int c_id = aux.cent_ind[i];
+		reg_reg[c_id].push_back(i);
 	}
 
 	return reg_reg;
 }
 
-vect_list lbg(vect_list v_train, int size, int N, int eps=.1){
-	vect_list centroids = new vect_list[N];
+inline vect_list up_centroids(vect_list v_train, reg_register reg_reg, int v_size){
+	vect_list centroids;//[reg_reg.size()];
+
+	for (vector< int > V_i : reg_reg){
+		int N = V_i.size();
+		int *cent = new int[v_size];
+		for (int i=0; i<v_size; i++)
+			cent[i] = 0;
+
+		for (int idx : V_i){
+			for (int i=0; i<v_size; i++)
+				cent[i] += v_train[idx][i]/N;
+			centroids.push_back(cent);
+		}
+	}
+
+	return centroids;
+}
+
+inline vect_list init_cent(vect_list vects, int n_cent, int dim){
+	vect_list centroids;
+	vector< int > pos;
+
+	for (int i=0; i<dim; i++)
+		pos.push_back(i);
+
+	sample(vects.begin(), vects.end(),
+			back_inserter(centroids), n_cent,
+			mt19937{random_device{}()});
+
+	for (int *y_i : centroids){
+		int pert = rand() % 2;
+		int pos = rand() % dim;						//selects a position
+		if ((pert == 0) or (y_i[pos] == 255))
+			pert = -1;
+		else if (y_i[pos] == 0)
+			pert = 1;
+		y_i[pos] += pert;
+	}
+
+	return centroids;
+}
+
+inline void print_centroids(vect_list centroids, int size){
+	for (int *y_i : centroids){
+		cout<<"y_i = [ ";
+		for (int i=0; i<size; i++)
+			cout<<y_i[i]<<" ";
+		cout<<"]"<<endl;
+	}
+}
+
+/* Using Introduction to Data Compression, by K. Sayood
+as reference. Pseudo-code can be found at pg. 309 (5th
+edition)*/
+vect_list lbg(vect_list v_train, int size, int N, float eps){
+	vect_list centroids;
 	int D_old, D=0;
 
 	//initialize centroids
+	centroids = init_cent(v_train, N, size);
+	print_centroids(centroids,size);
 
 	//initialize quantization region's auxiliary
 	lbg_aux aux;
-	int n_train = v_train.length();
-	aux->n_train = n_train;
-	aux->n_cent = N;
-	aux->cent_ind = new int[n_train];
-	aux->q_regions = new reg_register[N];
-	aux->sq_dist = new int[n_train];
+	int n_train = v_train.size();
+	aux.n_train = n_train;
+	aux.n_cent = N;
+	aux.cent_ind = new int[n_train];
+	/*aux.q_regions = new reg_register[N];*/
+	aux.sq_dist = new float[n_train];
 	for (int i=0; i<n_train; i++)
-		aux->sq_dist[i] = 3.4e+38;    //emulate inf dist
-	aux->size=size;
+		aux.sq_dist[i] = 3.4e+38;    //emulate inf dist
+	aux.size=size;
 
 	main_loop:
 		D_old = D;		//5
@@ -68,13 +127,26 @@ vect_list lbg(vect_list v_train, int size, int N, int eps=.1){
 		reg_register reg_reg = split_vects(aux);
 		for (vector< int > V_i : reg_reg){		//3
 			for (int idx : V_i)
-				D += aux->sq_dist[idx]
+				D += aux.sq_dist[idx];
 			D /= N;
 		}
 
 	if ((D - D_old)/D < eps)//4
 		return centroids;
 
-	//falta o 5 de atualizar os centroides
+	//5
+	/*delete centroids;*/
+	centroids = up_centroids(v_train, reg_reg, size);
 	goto main_loop;
+}
+
+
+int main(void){
+	vect_list vectors = {//ajeitar isso!!!
+		{1,2}, {1,1}, {0,1}, {2,2}, {2,1}, {3,4}, {4,2}, {5,2}, {2,4}
+	};
+	int n_cent = 3, size=2;
+	vect_list centr = lbg(vectors, n_cent, size);
+	print_centroids(centr, size);
+	return 0;
 }
